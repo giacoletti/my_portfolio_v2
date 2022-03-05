f9da3d48f52fb306a8375a617c3805008ebd6e6a
giacoletti <giacoletti92@gmail.com>
2022-03-05 17:42:38 +0100

    Add end2end test to check if visitor can see navigation bar with header, About Me button, Projects button, LinkedIn icon button, GitHub icon button

--- cypress/integration/userCanSeeMainView.feature.js
+++ cypress/integration/userCanSeeMainView.feature.js
@@ -9,4 +9,33 @@ describe("A user visiting the portfolio", () => {
       "Welcome to my portfolio"
     );
   });
+
+  describe("can see a navbar", () => {
+    it("is expected to see navbar element", () => {
+      cy.get("[data-cy=navbar]").should("be.visible");
+    });
+
+    it("is expected to contain 'Giovanni Iacoletti' header", () => {
+      cy.get("[data-cy=header-nav]").should(
+        "contain.text",
+        "Giovanni Iacoletti"
+      );
+    });
+
+    it("is expected to contain 'About Me' button", () => {
+      cy.get("[data-cy=about-nav]").should("contain.text", "About Me");
+    });
+
+    it("is expected to contain 'Projects' button", () => {
+      cy.get("[data-cy=projects-nav]").should("contain.text", "Projects");
+    });
+
+    it("is expected to contain 'LinkedIn' icon button", () => {
+      cy.get("[data-cy=linkedin-nav]").should("be.visible");
+    });
+
+    it("is expected to contain 'GitHub' icon button", () => {
+      cy.get("[data-cy=github-nav]").should("be.visible");
+    });
+  });
 });
