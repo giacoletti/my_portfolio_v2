9e4e608dc8a500be0d89d9a4824f635d1a5561cb
Giovanni Iacoletti <92155737+giacoletti@users.noreply.github.com>
2022-06-06 15:44:29 +0200

    Update end2end test: visitor can see mobile menu - is expected to not see About, Projects, Contact, LinkedIn and GitHub buttons

--- cypress/integration/visitorCanSeeMobileMenu.feature.js
+++ cypress/integration/visitorCanSeeMobileMenu.feature.js
@@ -1,10 +1,34 @@
 describe("A visitor visiting the portfolio on mobile", () => {
   before(() => {
-    cy.viewport("samsung-s10");
+    Cypress.config({
+      viewportWidth: 375,
+      viewportHeight: 812
+    });
+    cy.viewport(375, 812);
     cy.visit("/");
   });
-  
+
   it("is expected to see mobile menu icon in the navbar", () => {
     cy.get("[data-cy=mobile-menu-icon]").should("be.visible");
+  });
+
+  it("is expected to not see 'About Me' button", () => {
+    cy.get("[data-cy=about-nav]").should("not.exist");
+  });
+
+  it("is expected to not see 'Projects' button", () => {
+    cy.get("[data-cy=projects-nav]").should("not.exist");
+  });
+
+  it("is expected to not see 'Contact' button", () => {
+    cy.get("[data-cy=contact-nav]").should("not.exist");
+  });
+
+  it("is expected to not see 'LinkedIn' icon button", () => {
+    cy.get("[data-cy=linkedin-nav]").should("not.exist");
+  });
+
+  it("is expected to not see 'GitHub' icon button", () => {
+    cy.get("[data-cy=github-nav]").should("not.exist");
   });
 });
