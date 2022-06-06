4e6047b8266269729dd5e2e0f3f4327237af6241
Giovanni Iacoletti <92155737+giacoletti@users.noreply.github.com>
2022-06-06 22:00:58 +0200

    Add end2end test to check if visitor can click on mobile menu icon and click on 'About Me', 'Projects' and 'Contact' menu items to scroll to relative sections create menu items for mobile menu - 'About Me', 'Projects' and 'Contract'

--- cypress/integration/visitorCanSeeMobileMenu.feature.js
+++ cypress/integration/visitorCanSeeMobileMenu.feature.js
@@ -31,4 +31,56 @@ describe("A visitor visiting the portfolio on mobile", () => {
   it("is expected to not see 'GitHub' icon button", () => {
     cy.get("[data-cy=github-nav]").should("not.exist");
   });
+
+  describe("can click on the mobile menu icon and on 'About Me'", () => {
+    before(() => {
+      cy.get("[data-cy=mobile-menu-icon]").click();
+      cy.get("[data-cy=about-menu-item]").click();
+    });
+
+    it("is expected to scroll down to the 'About Me' section", () => {
+      cy.window()
+        .its("scrollY")
+        .should("equal", cy.$$("[data-cy=about-header]").offset().top - 100);
+    });
+
+    after(() => {
+      cy.get(".MuiBackdrop-root").click(); // close mobile menu by clicking invisible overlay
+    });
+  });
+
+  describe("can click on the mobile menu icon and on 'Projects'", () => {
+    before(() => {
+      cy.get("[data-cy=mobile-menu-icon]").click();
+      cy.get("[data-cy=projects-menu-item]").click();
+    });
+
+    it("is expected to scroll down to the 'Projects' section", () => {
+      cy.window()
+        .its("scrollY")
+        .should(
+          "equal",
+          Math.trunc(cy.$$("[data-cy=projects-header]").offset().top - 100)
+        );
+    });
+
+    after(() => {
+      cy.get(".MuiBackdrop-root").click();
+    });
+  });
+
+  describe("can click on the mobile menu icon and on 'Contact'", () => {
+    before(() => {
+      cy.get("[data-cy=mobile-menu-icon]").click();
+      cy.get("[data-cy=contact-menu-item]").click();
+    });
+
+    it("is expected to scroll down to the 'Contact' section", () => {
+      cy.get("[data-cy=contact-header]").should("be.inViewport");
+    });
+
+    after(() => {
+      cy.get(".MuiBackdrop-root").click();
+    });
+  });
 });
--- src/components/MobileDropdownMenu.jsx
+++ src/components/MobileDropdownMenu.jsx
@@ -1,13 +1,53 @@
-import React from "react";
-import IconButton from "@mui/material/IconButton";
-import MenuIcon from "@mui/icons-material/Menu";
+import React, { useState } from "react";
+import { IconButton, Menu, MenuItem } from "@mui/material";
+import { Menu as MenuIcon } from "@mui/icons-material";
+import { Link } from "react-scroll";
 
 const MobileDropdownMenu = () => {
+  const [anchorEl, setAnchorEl] = useState(null);
+  const isMenuOpen = Boolean(anchorEl);
+
+  const handleMenuOpen = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleMenuClose = () => {
+    setAnchorEl(null);
+  };
+
   return (
     <>
-      <IconButton data-cy="mobile-menu-icon" color="inherit">
+      <IconButton
+        data-cy="mobile-menu-icon"
+        onClick={handleMenuOpen}
+        color="inherit"
+      >
         <MenuIcon />
       </IconButton>
+      <Menu
+        anchorEl={anchorEl}
+        anchorOrigin={{
+          vertical: "top",
+          horizontal: "right"
+        }}
+        keepMounted
+        transformOrigin={{
+          vertical: "top",
+          horizontal: "right"
+        }}
+        open={isMenuOpen}
+        onClose={handleMenuClose}
+      >
+        <Link to="about" smooth={true} offset={-100}>
+          <MenuItem data-cy="about-menu-item">About Me</MenuItem>
+        </Link>
+        <Link to="projects" smooth={true} offset={-100}>
+          <MenuItem data-cy="projects-menu-item">Projects</MenuItem>
+        </Link>
+        <Link to="contact" smooth={true}>
+          <MenuItem data-cy="contact-menu-item">Contact</MenuItem>
+        </Link>
+      </Menu>
     </>
   );
 };
